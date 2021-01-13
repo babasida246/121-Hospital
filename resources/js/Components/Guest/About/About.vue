0c6902908a92ca15127260bad9a4c423dc135aa9
Do Minh Huy <dominhhuy200017@gmail.com>
2021-01-13 10:58:10 +0700

    Version 1.18

--- resources/js/Components/Guest/About/About.vue
+++ resources/js/Components/Guest/About/About.vue
@@ -11,7 +11,36 @@
           data-autoplay="true"
         ></a>
       </div>
-
+      <!-- <vs-col type="flex" vs-justify="center" vs-align="center" vs-w="5">
+        <vs-card>
+          <div slot="header">
+            <h3>Hello world !</h3>
+          </div>
+          <div>
+            <span
+              >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.</span
+            >
+          </div>
+          <div slot="footer">
+            <vs-row vs-justify="flex-end">
+              <vs-button
+                type="gradient"
+                color="danger"
+                icon="favorite"
+              ></vs-button>
+              <vs-button color="primary" icon="turned_in_not"></vs-button>
+              <vs-button
+                color="rgb(230,230,230)"
+                color-text="rgb(50,50,50)"
+                icon="settings"
+              ></vs-button>
+            </vs-row>
+          </div>
+        </vs-card>
+      </vs-col> -->
       <div
         class="col-xl-7 col-lg-6 icon-boxes d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-5"
       >
